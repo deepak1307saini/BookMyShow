1935f2efd6c81f7eac64db999f1b09aa0fb65ae6
Deepak Saini <deepak1307saini@gmail.com>
2023-03-06 14:19:12 +0530

    create

---
+++ src/main/java/com/example/BookMyShowApplication/BookMyShowApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookMyShowApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookMyShowApplication/BookMyShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookMyShowApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

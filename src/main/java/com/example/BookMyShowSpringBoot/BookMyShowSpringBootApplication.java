ce03554a9b61ea653ce67ff6dca98e0f5a75a108
Deepak Saini <deepak1307saini@gmail.com>
2023-03-11 21:40:12 +0530

    initialized

---
+++ src/main/java/com/example/BookMyShowSpringBoot/BookMyShowSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookMyShowSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookMyShowSpringBoot/BookMyShowSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookMyShowSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

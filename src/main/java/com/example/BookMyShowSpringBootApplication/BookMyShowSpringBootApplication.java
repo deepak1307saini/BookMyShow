b75a5a529d7cfa1996261d2d88b7d452a6ae2edb
Deepak Saini <deepak1307saini@gmail.com>
2023-03-11 21:44:54 +0530

    webSecurity added

--- src/main/java/com/example/BookMyShowSpringBootApplication/BookMyShowSpringBootApplication.java
+++ src/main/java/com/example/BookMyShowSpringBootApplication/BookMyShowSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.example.BookMyShowSpringBoot;
+package com.example.BookMyShowSpringBootApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/example/BookMyShowSpringBootApplication/config/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.BookMyShowSpringBootApplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class WebSecurityConfig{
+    private final String[] WHITE_LIST_URLS={
+            "/register",
+            "/login"
+    };
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(11);
+    }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .antMatchers(WHITE_LIST_URLS).permitAll();
+        return http.build();
+    }
+}
